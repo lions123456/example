57bb5f1608a9ce7a1b3aeb5a4535c3691f3a899f
lions123456 <155250809+lions123456@users.noreply.github.com>
2024-08-29 18:54:01 +0000

    second

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ appData = {
     savings : false,
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-a2 = prompt("Во сколько обойдется?", ''),
-a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-a4 = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+    }
+};
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.buget / 30;
+alert("");
